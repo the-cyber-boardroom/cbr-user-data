a32a9adc27972ec910aba444f3be01689ecb503e
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-24 13:04:58 +0100

    improved Routes__Session

--- cbr_user_data/fast_api/routes/Routes__Session.py
+++ cbr_user_data/fast_api/routes/Routes__Session.py
@@ -1,21 +1,24 @@
-from starlette.requests                                     import Request
-from cbr_shared.cbr_backend.users.S3_DB__Users              import S3_DB__Users
-from cbr_shared.cbr_backend.users.decorators.with_db_user   import with_db_user
-from osbot_fast_api.api.Fast_API_Routes                     import Fast_API_Routes
+from starlette.requests                                         import Request
+from cbr_shared.cbr_backend.session.S3_DB__Session              import S3_DB__Session
+from cbr_shared.cbr_backend.session.decorators.with_db_session  import with_db_session
+from cbr_shared.cbr_backend.users.S3_DB__Users                  import S3_DB__Users
+from osbot_fast_api.api.Fast_API_Routes                         import Fast_API_Routes
 
 class Routes__Session(Fast_API_Routes):
     tag     : str = 'session'
     db_users: S3_DB__Users
 
-    @with_db_user
-    def user_profile(self, request: Request):
-        db_user = request.state.db_user
-        return db_user.user_profile()
+    @with_db_session
+    def session_config(self, request: Request):
+        db_session : S3_DB__Session = request.state.db_session
+        return db_session.session_config()
 
+    @with_db_session
     def logged_in(self, request: Request):
-        return request.session.get('logged_in')
+        db_session : S3_DB__Session = request.state.db_session
+        return {"logged_in": db_session.exists()}
 
     def setup_routes(self):
-        self.add_route_get(self.logged_in   )
-        self.add_route_get(self.user_profile)
+        self.add_route_get(self.logged_in    )
+        self.add_route_get(self.session_config)
         return self
