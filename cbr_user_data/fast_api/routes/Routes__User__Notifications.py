c60b111d78fc0971209c9d5b44942c2898941a96
Dinis Cruz <dinis.cruz@owasp.org>
2024-11-21 17:01:41 +0000

    added use of new osbot-utils Generator_Manager class to Rules__User__Notifications stream

--- cbr_user_data/fast_api/routes/Routes__User__Notifications.py
+++ cbr_user_data/fast_api/routes/Routes__User__Notifications.py
@@ -1,19 +1,33 @@
-import json
 import time
-from fastapi                                                                import HTTPException
+from fastapi                                                                import HTTPException, BackgroundTasks
 from starlette.requests                                                     import Request
 from starlette.responses                                                    import StreamingResponse
-from typing                                                                 import AsyncGenerator
 from cbr_shared.cbr_backend.user_notifications.Model__User__Notification    import Model__User__Notification
 from cbr_shared.cbr_backend.user_notifications.User__Notifications          import User__Notifications
 from cbr_shared.cbr_backend.users.decorators.with_db_user                   import with_db_user
 from osbot_fast_api.api.Fast_API_Routes                                     import Fast_API_Routes
+from osbot_utils.helpers.generators.Generator_Manager                       import Generator_Manager
+from osbot_utils.helpers.generators.Model__Generator_State                  import Model__Generator_State
 from osbot_utils.utils.Json                                                 import json_to_str
 from osbot_utils.utils.Misc                                                 import wait_for
 
 
+
+
 class Routes__User__Notifications(Fast_API_Routes):
-    tag: str = 'notifications'
+    tag               : str                 = 'notifications'
+    generator_manager : Generator_Manager
+    count             : int
+
+    def generators__count(self):                                         # Return count of active SSE connections
+        return {'active_count': len(self.generator_manager.generators) }
+
+    def generators__stop_all(self):
+        return {'closed_count': self.generator_manager.stop_all() }
+
+    def generators__status(self):
+        return {'status': self.generator_manager.status() }
+
 
     def user_notifications(self, request: Request) -> User__Notifications:
         db_user = request.state.db_user
@@ -32,55 +46,64 @@ class Routes__User__Notifications(Fast_API_Routes):
             return {"status": "ok", "message": "Notification deleted"}
         raise HTTPException(status_code=404, detail="Notification not found")
 
-    # @with_db_user
-    # async def generate_events(self, request: Request) -> AsyncGenerator[str, None]:
-    #     last_check = time.time()
-    #     while True:
-    #         # Send heartbeat
-    #         yield f"event: heartbeat\ndata: {json.dumps({'timestamp': time.time()})}\n\n"
-    #
-    #         # Check for notifications
-    #         notifications = self.user_notifications(request).new(last_notification_timestamp=last_check)
-    #         if notifications:
-    #             yield f"event: notifications\ndata: {json.dumps([n.__dict__ for n in notifications])}\n\n"
-    #             self.user_notifications(request).mark_delivered([n.notification_id for n in notifications])
-    #
-    #         last_check = time.time()
-    #         await asyncio.sleep(1)
-    #
-    # @with_db_user
-    # async def live_stream(self, request: Request):
-    #     return StreamingResponse(self.generate_events(request), media_type="text/event-stream")
+    def generate_events(self, request: Request, wait_count=100, wait_time=1, get_generator=None):
 
-
-    def generate_events(self, request: Request, wait_count=100, wait_time=1):
         user_notifications = self.user_notifications(request)
         last_check         = time.time()
-        while wait_count > 0:
+        generator          = get_generator()
+        try:
+            while wait_count > 0:
+                if generator.state != Model__Generator_State.RUNNING:
+                    print(f'>>> stopping generator: {generator}')
+                    return
+                if request.scope.get('state').get('is_disconnected') :
+                    print(">>> disconnected detected, from request.scope.get('state')")
+                    return
 
-            event = {'count': wait_count,                                                           # Send heartbeat
-                     'event':'heartbeat',
-                     'data': {'timestamp': time.time()}}
-            yield json_to_str(event)
+                event = {'count': wait_count,                                                           # Send heartbeat
+                         'event':'heartbeat',
+                         'data': {'timestamp': time.time()}}
 
-            notifications     = user_notifications.new(last_notification_timestamp=last_check)      # Check for notifications
-            notifications_ids = []
-            if notifications:
-                for notification in notifications:
-                    notifications_ids.append(notification.notification_id)
-                    event = {'count' : wait_count,
-                             'event': 'notification',
-                             'data' : notification.json()}
-                    yield json_to_str(event)
-                user_notifications.mark_delivered(notifications_ids)
+                try:
+                    data = json_to_str(event)
+                    yield data
+                except Exception as exception:
+                    print(f'--- yield exception: {exception}')
 
-            last_check = time.time()
-            wait_for(wait_time)
-            wait_count -= 1
+                notifications     = user_notifications.new(last_notification_timestamp=last_check)      # Check for notifications
+                notifications_ids = []
+                if notifications:
+                    for notification in notifications:
+                        notifications_ids.append(notification.notification_id)
+                        event = {'count' : wait_count,
+                                 'event': 'notification',
+                                 'data' : notification.json()}
+                        yield json_to_str(event)
+                    user_notifications.mark_delivered(notifications_ids)
+
+                last_check = time.time()
+                wait_for(wait_time)
+                wait_count -= 1
+        finally:
+            print('>>> completed')
+            generator.state = Model__Generator_State.COMPLETED
+
+
 
     @with_db_user
     def live_stream(self, request: Request, wait_count=50, wait_time=1):
-        return StreamingResponse(self.generate_events(request, int(wait_count), float(wait_time)), media_type="text/event-stream")
+
+        def get_generator():
+            nonlocal target_id
+            generator = self.generator_manager.generator(target_id)
+            return generator
+
+        gen            = self.generate_events(request, int(wait_count), float(wait_time), get_generator)
+        target_id      = self.generator_manager.add(gen)
+
+        return StreamingResponse(gen, media_type="text/event-stream")
+
+
 
 
     @with_db_user
@@ -94,11 +117,15 @@ class Routes__User__Notifications(Fast_API_Routes):
         return {"notifications": [n.__dict__ for n in notifications]}
 
     def setup_routes(self):
-        self.add_route_post  (self.create     )
-        self.add_route_delete(self.delete     )
-        self.add_route_get   (self.live_stream)
-        self.add_route_get   (self.current    )
-        self.add_route_get   (self.all        )
+        self.add_route_post  (self.create              )
+        self.add_route_delete(self.delete              )
+        self.add_route_get   (self.live_stream         )
+        self.add_route_get   (self.current             )
+        self.add_route_get   (self.all                 )
+        self.add_route_get   (self.generators__count   )
+        self.add_route_get   (self.generators__stop_all)
+        self.add_route_get   (self.generators__status  )
+
 
 
 
